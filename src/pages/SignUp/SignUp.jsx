2c6d32c777078c38827dc3a912acf3e3cc030523
SirLucasLima <doislcompany@gmail.com>
2023-01-03 03:07:28 -0300

    signup

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,16 +1,21 @@
 import { Background, Container, Form } from "./styles";
-import { FiMail, FiLock} from "react-icons/fi"
+import { FiMail, FiLock, FiUser } from "react-icons/fi"
 import { Input } from "../../components/Input/Input";
 import { Button } from "../../components/Button/Button";
 
-export default function SignIn(){
+export default function SignUp(){
   return(
     <Container>
+      <Background />
       <Form>
         <h1>Notes</h1>
         <p>App to save and manage your asufel links and notes</p>
-        <h2>Sign In</h2>
-
+        <h2>Sign Up</h2>
+        <Input 
+          placeholder="Name"
+          type="password"
+          icon={ FiUser }
+        />
         <Input 
           placeholder="E-mail"
           type="text"
@@ -21,10 +26,9 @@ export default function SignIn(){
           type="password"
           icon={ FiLock }
         />
-        <Button title="Sign In" />
-        <a href="#">Sign Up</a>
+        <Button title="Sign Up" />
+        <a href="#">Back to sign in</a>
       </Form>
-      <Background />
     </Container>
   )
 }
