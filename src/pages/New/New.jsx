50d464d5295053defe720df5b6a30105f141bc3c
SirLucasLima <doislcompany@gmail.com>
2023-01-05 04:03:16 -0300

    newiItem

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -1,5 +1,8 @@
 import { Header } from "../../components/Header/Header";
 import { Input } from "../../components/Input/Input";
+import { NewItem } from "../../components/NewItem/NewItem";
+import { Section } from "../../components/Section/Section";
+import { TextArea } from "../../components/TextArea/TextArea";
 import { Container, Form } from "./styles";
 
 export default function New(){
@@ -14,9 +17,13 @@ export default function New(){
             <a href="">return</a>
           </header>
 
-          <Input
-            placeholder="Title"
-          />
+          <Input placeholder="Title" />
+          <TextArea placeholder="Comments"/>
+
+          <Section title="Useful links">
+            <NewItem value="https://app.rocketseat.com.br/node/stage-09/group/desenvolvendo-aplicacao/lesson/estilizando-o-note-item-1"/>
+            <NewItem isNew placeholder="New link"/>
+          </Section>
         </Form>
       </main>
     </Container>
